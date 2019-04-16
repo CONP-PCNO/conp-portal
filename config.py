720bd6d8adcbe1216758dc93f1839e9ba6328738
santiago <paiva.santiago@gmail.com>
2019-04-15 22:02:49 -0400

    modify data path

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    DATA_PATH = '/data'
+    DATA_PATH = '/data/projects'
     SQLALCHEMY_DATABASE_URI = (os.environ.get('DATABASE_URL') or
                                'postgresql://localhost/conp')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
