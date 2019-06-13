d46993f42bbc739cc6445cd19b30f87b490fc8c9
joeyzhou98 <joeyzhou7002@gmail.com>
2019-06-12 20:33:00 -0400

    minor touchups

--- app/static/lib/conp-react/src/DatasetElement/index.js
+++ app/static/lib/conp-react/src/DatasetElement/index.js
@@ -83,7 +83,6 @@ const DatasetElement = props => {
                 pointerEvents: element.isPrivate && !authorized ? "none" : "all"
               }}
             >
-            >
               <img
                 alt="Run On Cbrain"
                 className="run-on-cbrain-button option-icon"
