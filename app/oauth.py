28c9f783fca8c356fb9e45330cfd4442ab7632be
Liam O'Callaghan <liam@liamocallaghan.com>
2019-08-28 14:04:53 -0400

    reference blueprint url in OauthSignIn

--- app/oauth.py
+++ app/oauth.py
@@ -20,7 +20,7 @@ class OAuthSignIn(object):
         pass
 
     def get_callback_url(self):
-        return url_for('oauth_callback', provider=self.provider_name,
+        return url_for('auth.oauth_callback', provider=self.provider_name,
                        _external=True)
 
     @classmethod
