1bd7dd6598266a0f9ce71b99d41b7bd65c0951c8
Liam O'Callaghan <liam@liamocallaghan.com>
2019-09-23 13:44:51 -0400

    remove get checks from api implementation

--- app/pipelines/routes.py
+++ app/pipelines/routes.py
@@ -39,89 +39,88 @@ def pipeline_search():
         Returns:
             JSON of the applicable pipelines
     """
-    if request.method == 'GET':
 
-        authorized = True if current_user.is_authenticated else False
+    authorized = True if current_user.is_authenticated else False
 
-        # initialize variables
-        search_query = request.args.get("search").lower() if request.args.get("search") else ''
-        sort_key = request.args.get("sortKey") or "downloads-desc"
-        cache_dir = os.path.join(os.path.expanduser('~'), ".cache", "boutiques")
-        all_desc_path = os.path.join(cache_dir, "all_descriptors.json")
-        all_detailed_desc_path = os.path.join(cache_dir, "detailed_all_descriptors.json")
+    # initialize variables
+    search_query = request.args.get("search").lower() if request.args.get("search") else ''
+    sort_key = request.args.get("sortKey") or "downloads-desc"
+    cache_dir = os.path.join(os.path.expanduser('~'), ".cache", "boutiques")
+    all_desc_path = os.path.join(cache_dir, "all_descriptors.json")
+    all_detailed_desc_path = os.path.join(cache_dir, "detailed_all_descriptors.json")
 
-        # fetch data from cache
-        with open(all_desc_path, "r") as f:
-            all_descriptors = json.load(f)
+    # fetch data from cache
+    with open(all_desc_path, "r") as f:
+        all_descriptors = json.load(f)
 
-        with open(all_detailed_desc_path, "r") as f:
-            detailed_all_descriptors = json.load(f)
+    with open(all_detailed_desc_path, "r") as f:
+        detailed_all_descriptors = json.load(f)
 
-        # if the cache data older than 5min or 300 seconds, update in background
-        delta = time.time() - os.path.getmtime(all_desc_path)
-        if delta > 300:
-            print("Pipeline database older than 5 minutes, updating...")
-            UpdatePipelineData(path=cache_dir).start()
+    # if the cache data older than 5min or 300 seconds, update in background
+    delta = time.time() - os.path.getmtime(all_desc_path)
+    if delta > 300:
+        print("Pipeline database older than 5 minutes, updating...")
+        UpdatePipelineData(path=cache_dir).start()
 
-        # search cache with search query else return everything
-        if search_query not in ("", '', None):
-            elements = [
-                {**descriptor, **detailed_all_descriptors[d_index]}
-                for d_index, descriptor in enumerate(all_descriptors)
-                if search_query in (
-                    (str(descriptor.values())
-                        + str(detailed_all_descriptors[d_index]["tags"].values())).lower()
-                    if "tags" in detailed_all_descriptors[d_index] else
-                    str(descriptor.values()).lower()
-                )
-            ]
-        else:
-            elements = [
-                {**descriptor, **detailed_all_descriptors[d_index]}
-                for d_index, descriptor in enumerate(all_descriptors)
-            ]
+    # search cache with search query else return everything
+    if search_query not in ("", '', None):
+        elements = [
+            {**descriptor, **detailed_all_descriptors[d_index]}
+            for d_index, descriptor in enumerate(all_descriptors)
+            if search_query in (
+                (str(descriptor.values())
+                    + str(detailed_all_descriptors[d_index]["tags"].values())).lower()
+                if "tags" in detailed_all_descriptors[d_index] else
+                str(descriptor.values()).lower()
+            )
+        ]
+    else:
+        elements = [
+            {**descriptor, **detailed_all_descriptors[d_index]}
+            for d_index, descriptor in enumerate(all_descriptors)
+        ]
 
-        # sort, make all keys lowercase and without hyphen
-        elements = [{k.lower().replace("-", ""): v for k, v in element.items()}
-                    for element in elements]
-        elements.sort(
-            key=lambda x: x["downloads"],
-            reverse=True if sort_key == "downloads-desc" or sort_key == "title" else False
-        )
+    # sort, make all keys lowercase and without hyphen
+    elements = [{k.lower().replace("-", ""): v for k, v in element.items()}
+                for element in elements]
+    elements.sort(
+        key=lambda x: x["downloads"],
+        reverse=True if sort_key == "downloads-desc" or sort_key == "title" else False
+    )
 
-        # if element has online platform url, retrieve the cbrain one,
-        # else take the first one and set logo
-        for element in elements:
-            if "onlineplatformurls" in element:
-                for url in element["onlineplatformurls"]:
-                    if "cbrain" in url:
-                        element["onlineplatformurls"] = url
-                        element["img"] = "static/img/run_on_cbrain_green.png"
-                        break
-                else:
-                    element["onlineplatformurls"] = element["onlineplatformurls"][0]
-                    element["img"] = "static/img/globe-solid-green.png"
+    # if element has online platform url, retrieve the cbrain one,
+    # else take the first one and set logo
+    for element in elements:
+        if "onlineplatformurls" in element:
+            for url in element["onlineplatformurls"]:
+                if "cbrain" in url:
+                    element["onlineplatformurls"] = url
+                    element["img"] = "static/img/run_on_cbrain_green.png"
+                    break
             else:
-                element["img"] = "static/img/globe-solid-grey.png"
+                element["onlineplatformurls"] = element["onlineplatformurls"][0]
+                element["img"] = "static/img/globe-solid-green.png"
+        else:
+            element["img"] = "static/img/globe-solid-grey.png"
 
-        # construct payload
-        payload = {
-            "authorized": authorized,
-            "total": len(elements),
-            "sortKeys": [
-                {
-                    "key": "downloads-desc",
-                    "label": "Downloads: High to Low"
-                },
-                {
-                    "key": "downloads-asc",
-                    "label": "Downloads: Low to High"
-                }
-            ],
-            "elements": elements
-        }
+    # construct payload
+    payload = {
+        "authorized": authorized,
+        "total": len(elements),
+        "sortKeys": [
+            {
+                "key": "downloads-desc",
+                "label": "Downloads: High to Low"
+            },
+            {
+                "key": "downloads-asc",
+                "label": "Downloads: Low to High"
+            }
+        ],
+        "elements": elements
+    }
 
-        return json.dumps(payload)
+    return json.dumps(payload)
 
 
 @pipelines_bp.route('/share')
