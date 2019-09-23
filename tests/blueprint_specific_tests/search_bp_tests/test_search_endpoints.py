879d448f782af63cd4e52fe8e831af7b2f3017fa
Liam O'Callaghan <liam@liamocallaghan.com>
2019-09-23 12:46:31 -0400

    add tests for search response body

--- tests/blueprint_specific_tests/search_bp_tests/test_search_endpoints.py
+++ tests/blueprint_specific_tests/search_bp_tests/test_search_endpoints.py
@@ -21,9 +21,8 @@ def test_dataset_search_route(session, new_dataset, new_dataset_stats, test_clie
     """
     GIVEN calling the route "/dataset-search"
     WHEN no user is logged in
-    THEN should return success code
-
-    TODO: mock the datasets table to provide a valid return object
+    AND no search filter is used
+    THEN should return success code with all datasets
     """
 
     session.add(new_dataset)
@@ -32,11 +31,42 @@ def test_dataset_search_route(session, new_dataset, new_dataset_stats, test_clie
     session.add(new_dataset_stats)
     session.commit()
 
-    res = test_client.get("/dataset-search")
+    headers = {'Content-Type': 'application/json'}
+    res = test_client.get("/dataset-search", headers = headers)
     assert res.status_code == 200
 
+    body = res.get_json(force=True)
 
-def test_dataset_route(test_client):
+    assert type(body) != type(None)
+    assert body["authorized"] == False
+    assert body["total"] == 1
+
+def test_dataset_search_route_with_filter(session, new_dataset, new_dataset_stats, test_client):
+    """
+    GIVEN calling the route "/dataset-search"
+    WHEN no user is logged in
+    AND a filter is used
+    THEN should return success code with filtered results
+    """
+
+    session.add(new_dataset)
+    session.commit()
+
+    session.add(new_dataset_stats)
+    session.commit()
+
+    query = {'search': 'SearchTerm'}
+    headers = {'Content-Type': 'application/json'}
+    res = test_client.get("/dataset-search", headers = headers, query_string = query)
+    assert res.status_code == 200
+
+    body = res.get_json(force=True)
+
+    assert type(body) != type(None)
+    assert body["total"] == 0
+
+
+def test_dataset_route(session, new_dataset, new_dataset_stats, test_client):
     """
     GIVEN calling the route "/dataset"
     WHEN no user is logged in
@@ -44,7 +74,15 @@ def test_dataset_route(test_client):
 
     TODO: mock the datasets table to provide a valid return object
     """
-    #res = test_client.get("/dataset")
-    #assert res.status_code == 200
+    session.add(new_dataset)
+    session.commit()
+
+    session.add(new_dataset_stats)
+    session.commit()
+
+    query = {'id': '8de99b0e-5f94-11e9-9e05-52545e9add8e'}
+    headers = {'Content-Type': 'application/json'}
+
+    # res = test_client.get("/dataset", headers = headers, query_string = query)
 
     assert True
