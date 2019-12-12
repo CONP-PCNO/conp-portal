ca9d4d6b4353f57e386e507c531bdfd09becd2d7
Liam O'Callaghan <liam@liamocallaghan.com>
2019-12-12 17:22:30 -0500

    draw months from datasets and pipelines

--- app/static/lib/conp-react/src/DashboardChart/index.js
+++ app/static/lib/conp-react/src/DashboardChart/index.js
@@ -12,6 +12,23 @@ const DashboardChart = ({ datasetsURL, pipelinesURL, ...props }) => {
 
     const drawChart = (data) => {
         console.log('drawing chart');
+
+        const xAxis = [];
+        const yAxisDatasets = [];
+        const yAxisPipelines = [];
+
+        Object.keys(data.datasets).forEach(year => {
+            Object.keys(data.datasets[year]).forEach(month => {
+                xAxis.push(`${month}/${year}`);
+                yAxisDatasets.push(data.datasets[year][month]);
+                yAxisPipelines.push(data.pipelines[year][month] ? data.pipelines[year][month] : 0);
+            });
+        });
+
+        console.log(xAxis);
+        console.log(yAxisDatasets);
+        console.log(yAxisPipelines);
+
         Highcharts.chart('dashboard-chart-container', {
 
             chart: {
@@ -30,18 +47,18 @@ const DashboardChart = ({ datasetsURL, pipelinesURL, ...props }) => {
             yAxis: [{
                 className: 'highcharts-color-0',
                 title: {
-                    text: 'Number of Pipelines'
+                    text: 'Number of Datasets'
                 }
             }, {
                 className: 'highcharts-color-1',
                 opposite: true,
                 title: {
-                    text: 'Number of Datasets'
+                    text: 'Number of Pipelines'
                 }
             }],
 
             xAxis: {
-                categories: ["January'19", "February'19", "March'19", "April'19"]
+                categories: xAxis
             },
 
             plotOptions: {
@@ -54,10 +71,10 @@ const DashboardChart = ({ datasetsURL, pipelinesURL, ...props }) => {
 
             series: [{
                 name: 'CONP Datasets',
-                data: [0, 0, 0, data.datasetsTotal]
+                data: yAxisDatasets
             }, {
                 name: 'CONP Pipelines',
-                data: [0, 0, 0, data.pipelinesTotal],
+                data: yAxisPipelines,
                 yAxis: 1
             }]
 
@@ -78,10 +95,29 @@ const DashboardChart = ({ datasetsURL, pipelinesURL, ...props }) => {
 
             const datasetsRes = await res.json();
 
-            console.log(JSON.stringify(datasetsRes.total));
+            console.log(JSON.stringify(datasetsRes));
 
-            //setFetchedElements(parsed.elements);
-            //setTotalState(parsed.total);
+            const chartData = {
+                datasets: {},
+                pipelines: {}
+            };
+
+            datasetsRes.elements.map(element => {
+                const dateAdded = new Date(element.dateAdded);
+
+                if(!chartData.datasets[dateAdded.getFullYear()]){
+                    chartData.datasets[dateAdded.getFullYear()] = {}
+                }
+
+                if (!chartData.datasets[dateAdded.getFullYear()][dateAdded.getMonth() + 1]) {
+                    chartData.datasets[dateAdded.getFullYear()][dateAdded.getMonth() + 1] = 1;
+                }
+                else {
+                    chartData.datasets[dateAdded.getFullYear()][dateAdded.getMonth() + 1] += 1
+                }
+            })
+
+            console.log(chartData);
 
             console.log(`Fetching from: ${pipelinesURL}`);
             const pipelines = await fetch(pipelinesURL);
@@ -94,14 +130,13 @@ const DashboardChart = ({ datasetsURL, pipelinesURL, ...props }) => {
 
             const pipelinesRes = await pipelines.json();
 
-            console.log(JSON.stringify(pipelinesRes.elements.length));
+            console.log(JSON.stringify(pipelinesRes));
 
-            const data = {
-                datasetsTotal: datasetsRes.total,
-                pipelinesTotal: pipelinesRes.elements.length
-            };
+            chartData.pipelines[2019] = {};
 
-            drawChart(data);
+            chartData.pipelines[2019][12] = pipelinesRes.elements.length;
+
+            drawChart(chartData);
 
         } catch (err) {
             alert("There was an error retrieving the search results.");
