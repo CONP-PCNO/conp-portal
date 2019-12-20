de1ff8b978ea9cc3d142fe01543df8ab8c2ca717
xlecours <xavier.lecoursboucher@mcgill.ca>
2019-12-20 13:56:39 -0500

    adjusting study spotligth to show me description and logo

--- app/static/lib/conp-react/src/Spotlight/index.js
+++ app/static/lib/conp-react/src/Spotlight/index.js
@@ -40,14 +40,23 @@ const Spotlight = ({ datasetURL, ...props }) => {
                 <hr />
 
                 <div className="card-description-text">
+                    <div className="col-xs-12 col-sm-6 col-md-3 col-lg-2 card-img card-social">
+                      <img
+                        alt="Project image"
+                        className="card-img-top card-social-img"
+                        src="/dataset_logo?id=projects/preventad-open"
+                      />
+                    </div>
                     <p className="card-description-subtitle">
                         Prevent-AD
                 </p>
-                    <i>
-                        "StoP-AD Center - Douglas Mental Health University Institute"
-                </i>
                     <p>
-                        "Longitudinal multimodal study of pre-symptomatic Alzheimer's Disease"
+The PREVENT-AD (Pre-symptomatic Evaluation of Experimental or Novel Treatments for Alzheimer Disease) cohort is composed of cognitively healthy participants over 55 years old, at risk of developing Alzheimer Disease (AD) as their parents and/or siblings were/are affected by the disease. These ‘at-risk’ participants have been followed for a naturalistic study of the presymptomatic phase of AD since 2011 using multimodal measurements of various disease indicators. Two clinical trials intended to test pharmaco-preventive agents have also been conducted.
+
+The PREVENT-AD research group is now releasing data openly with the intention to contribute to the community’s growing understanding of AD pathogenesis. 
+                    </p>
+                    <p>
+                        Authors: StoP-AD Center - Douglas Mental Health University Institute
                 </p>
                 </div>
                 <div className="d-flex mt-4 justify-content-end">
