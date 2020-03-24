7c4c44290475fb54952cb238fb87a253894ca24f
John Saigle <4022790+johnsaigle@users.noreply.github.com>
2020-03-24 16:26:11 -0400

    add security verification (#199)
    * add security verification
    
    * request.data
    
    hmac.compare_digest
    
    * remove unused json library
    
    Co-authored-by: xlecours <xavier.lecoursboucher@mcgill.ca>

--- app/webhooks/routes.py
+++ app/webhooks/routes.py
@@ -6,11 +6,15 @@
 from app import config
 from app.webhooks import webhooks_bp
 from flask import request, abort, current_app
+from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 import git
+import hmac
 import os
+import hashlib
 
-@webhooks_bp.route('/webhooks', methods=['POST'])
+
+@webhooks_bp.route('/webhooks', methods=['GET', 'POST'])
 def webhooks():
     """ Github webhooks
 
@@ -21,10 +25,25 @@ def webhooks():
             JSON empty
     """
     if not request.is_json:
-        abort(404)
+        abort(400)
 
-    payload = request.get_json()
+    """ Security check. GitHub will send a header containing an HMAC of the 
+        contents of the POST body. We calculate this server side and update the
+        code only if the hashes match.
 
+        See: https://developer.github.com/webhooks/securing/
+    """
+    server_signature = 'sha1=' + hmac.new(
+        bytes(current_app.config['WEBHOOKS_SECRET'], 'ascii'),
+        request.data,
+        digestmod=hashlib.sha1
+    ).hexdigest()
+
+    client_signature = request.headers['X-Hub-Signature']
+
+    if not hmac.compare_digest(server_signature, client_signature):
+        abort(400)
+            
     # Initialize the git repository object
     repo = git.Repo(os.getcwd())
 
@@ -33,4 +52,3 @@ def webhooks():
     origin.pull('master')
 
     return 'OK'
-
