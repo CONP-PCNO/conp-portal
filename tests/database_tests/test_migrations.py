0eb7fff0de3c378662b31863b9bacb28a2b1f3da
joeyzhou98 <joeyzhou7002@gmail.com>
2020-07-30 21:12:46 -0400

    test

--- tests/database_tests/test_migrations.py
+++ tests/database_tests/test_migrations.py
@@ -43,6 +43,7 @@ def test_seed_db_test(app, session, runner):
     """
     cli.register(app)
     result = runner.invoke(args=["seed_test_db"])
+    print(result)
     assert result.exit_code == 0, result.output
 
     d = session.query(Dataset).all()
