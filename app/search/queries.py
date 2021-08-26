0f6243ea4a7bc47bd07461086205d34f117652c6
zxenia <ksenia1zaytseva@gmail.com>
2021-08-26 17:00:28 -0400

    add more example queries

--- app/search/queries.py
+++ app/search/queries.py
@@ -1,39 +1,69 @@
 # The file contains example SPARQL queries that populate SPARQL query editor
 
-example_query_1 = """# The query
-PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-SELECT * WHERE {
-    ?sub ?pred ?obj .
-} LIMIT 50
+example_query_1 = """# The query searches for all datasets that are licensed under the CC BY-NC-SA license.
+
+PREFIX sdo: <https://schema.org/>
+SELECT DISTINCT ?title ?license_name WHERE {
+?dataset a sdo:Dataset;
+  sdo:name ?title;
+  sdo:license ?license.
+  ?license sdo:name ?license_name.
+FILTER (?license_name = "CC BY-NC-SA"^^sdo:Text)
+}
 """
 
-example_query_2 = """# The query looks for all datasets that annotated with a concept Homo sapiens
-PREFIX sdo: <https://schema.org/>
-SELECT DISTINCT ?datasetname ?conceptname ?uri
-WHERE {
-?dataset a sdo:Dataset;
-    sdo:name ?datasetname;
-    sdo:about ?about.  
-OPTIONAL {
-   ?about sdo:name ?conceptname.
-}
-OPTIONAL {
-    { 
-    ?concept sdo:identifier ?identifier_val;
-        sdo:Property ?uri.
-    } 
-    UNION 
-    { 
-    ?concept sdo:value ?annotation_val;
-        sdo:url ?uri.
-    }
+example_query_2 = """# The query searches for all datasets that are about Alzheimer's.
 
-    ?conceptroot (<>|!<>) ?concept;
-        sdo:name ?conceptname.
-    ?about (<>|!<>)* ?conceptroot.
+PREFIX sdo: <https://schema.org/>
+PREFIX conp: <https://reservoir.global/v1/vocabs/Public/CONP/>
+SELECT DISTINCT ?dataset ?title ?about_name WHERE {
+?dataset a sdo:Dataset;
+  sdo:name ?title;
+  sdo:about ?about.
+?about sdo:name ?about_name.         
+FILTER regex(lcase(str(?about_name)), "alzheimer", "i")
 }
-FILTER (?conceptname = "Homo sapiens"^^sdo:Text)
-FILTER NOT EXISTS { ?otherdata a sdo:Dataset; sdo:hasPart ?dataset }
+"""
+
+example_query_3 = """# The query searches for all datasets that have distribution in the MINC file format.
+
+PREFIX sdo: <https://schema.org/>
+PREFIX conp: <https://reservoir.global/v1/vocabs/Public/CONP/>
+SELECT DISTINCT ?dataset ?title ?format WHERE {
+?dataset a sdo:Dataset;
+  sdo:name ?title;
+  sdo:distribution ?distribution.
+  ?distribution conp:formats ?format.
+FILTER (regex(?format, "MINC", "i"))
+}"""
+
+example_query_4 = """# This query search for datasets that have an authorization type set to "Public".
+
+PREFIX sdo: <https://schema.org/>
+PREFIX conp: <https://reservoir.global/v1/vocabs/Public/CONP/>
+SELECT DISTINCT ?dataset_name ?value
+WHERE {
+    ?dataset a sdo:Dataset;
+        sdo:name ?dataset_name;
+        sdo:distribution ?distribution.
+  ?distribution sdo:accessMode ?access_mode.
+  ?access_mode conp:authorizations ?authorization.
+  ?authorization sdo:value ?value.
+FILTER regex(lcase(str(?value)), "public", "i")
+}
+"""
+
+example_query_5 = """# This query returns all datasets and their content size.
+
+PREFIX sdo: <https://schema.org/>
+PREFIX conp: <https://reservoir.global/v1/vocabs/Public/CONP/>
+SELECT DISTINCT ?dataset_name ?content_size ?value
+WHERE {
+    ?dataset a sdo:Dataset;
+        sdo:name ?dataset_name;
+        sdo:distribution ?distribution.
+  ?distribution sdo:contentSize ?content_size;
+                conp:unit ?unit.
+  ?unit sdo:value ?value.
 }
 """
