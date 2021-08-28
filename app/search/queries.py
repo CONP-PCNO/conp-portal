0331d1521e3d5349ec938b0a6e83a1f6d3edab0c
zxenia <ksenia1zaytseva@gmail.com>
2021-08-27 20:07:11 -0400

    minor style improvement

--- app/search/queries.py
+++ app/search/queries.py
@@ -7,7 +7,7 @@ SELECT DISTINCT ?title ?license_name WHERE {
 ?dataset a sdo:Dataset;
   sdo:name ?title;
   sdo:license ?license.
-  ?license sdo:name ?license_name.
+?license sdo:name ?license_name.
 FILTER (?license_name = "CC BY-NC-SA"^^sdo:Text)
 }
 """
@@ -32,22 +32,22 @@ SELECT DISTINCT ?dataset ?title ?format WHERE {
 ?dataset a sdo:Dataset;
   sdo:name ?title;
   sdo:distribution ?distribution.
-  ?distribution conp:formats ?format.
+?distribution conp:formats ?format.
 FILTER (regex(?format, "MINC", "i"))
-}"""
+}
+"""
 
 example_query_4 = """# The query searches for datasets that have an authorization type set to "Public".
 
 PREFIX sdo: <https://schema.org/>
 PREFIX conp: <https://reservoir.global/v1/vocabs/Public/CONP/>
-SELECT DISTINCT ?dataset_name ?value
-WHERE {
-    ?dataset a sdo:Dataset;
-        sdo:name ?dataset_name;
-        sdo:distribution ?distribution.
-  ?distribution sdo:accessMode ?access_mode.
-  ?access_mode conp:authorizations ?authorization.
-  ?authorization sdo:value ?value.
+SELECT DISTINCT ?dataset_name ?value WHERE {
+?dataset a sdo:Dataset;
+  sdo:name ?dataset_name;
+  sdo:distribution ?distribution.
+?distribution sdo:accessMode ?access_mode.
+?access_mode conp:authorizations ?authorization.
+?authorization sdo:value ?value.
 FILTER regex(lcase(str(?value)), "public", "i")
 }
 """
@@ -55,13 +55,15 @@ FILTER regex(lcase(str(?value)), "public", "i")
 example_query_5 = """# The query returns a list of cited papers (including their doi) with datasets that cited them.
 
 PREFIX sdo: <https://schema.org/>
-SELECT DISTINCT ?citation_name ?doi (GROUP_CONCAT(DISTINCT ?title; separator=" | ") as ?datasets) (COUNT(DISTINCT ?title) as ?citation_count) WHERE {
+SELECT DISTINCT ?citation_name ?doi
+(GROUP_CONCAT(DISTINCT ?title; separator=" | ") as ?datasets) 
+(COUNT(DISTINCT ?title) as ?citation_count) WHERE {
 ?dataset a sdo:Dataset;
   sdo:name ?title;
   sdo:citation ?citation.
-  ?citation sdo:identifier ?value;
-            sdo:name ?citation_name.
-  ?value sdo:identifier ?doi
+?citation sdo:identifier ?value;
+          sdo:name ?citation_name.
+?value sdo:identifier ?doi.
 }
 GROUP BY ?citation_name ?doi
 """
