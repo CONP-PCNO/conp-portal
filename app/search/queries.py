c8a108545a908c5394921f9325f17671f314ee7a
surchs <sebastian.urchs@gmail.com>
2021-12-22 14:06:33 -0500

    Implement nexus deprecation flag

--- app/search/queries.py
+++ app/search/queries.py
@@ -3,9 +3,12 @@
 example_query_1 = """# The query searches for all datasets that are licensed under the CC BY-NC-SA license.
 
 PREFIX sdo: <https://schema.org/>
+PREFIX nexus: <https://bluebrain.github.io/nexus/vocabulary/>
+
 SELECT DISTINCT ?title ?license_name WHERE {
 ?dataset a sdo:Dataset;
   sdo:name ?title;
+  nexus:deprecated false;
   sdo:license ?license.
 ?license sdo:name ?license_name.
 FILTER (?license_name = "CC BY-NC-SA"^^sdo:Text)
@@ -15,9 +18,12 @@ FILTER (?license_name = "CC BY-NC-SA"^^sdo:Text)
 example_query_2 = """# The query searches for all datasets that are about Alzheimer's disease.
 
 PREFIX sdo: <https://schema.org/>
+PREFIX nexus: <https://bluebrain.github.io/nexus/vocabulary/>
+
 SELECT DISTINCT ?dataset ?title ?about_name WHERE {
 ?dataset a sdo:Dataset;
   sdo:name ?title;
+  nexus:deprecated false;
   sdo:about ?about.
 ?about sdo:name ?about_name.         
 FILTER regex(lcase(str(?about_name)), "alzheimer", "i")
@@ -28,9 +34,12 @@ example_query_3 = """# The query searches for all datasets that have distributio
 
 PREFIX sdo: <https://schema.org/>
 PREFIX conp: <https://reservoir.global/v1/vocabs/Public/CONP/>
+PREFIX nexus: <https://bluebrain.github.io/nexus/vocabulary/>
+
 SELECT DISTINCT ?dataset ?title ?format WHERE {
 ?dataset a sdo:Dataset;
   sdo:name ?title;
+  nexus:deprecated false;
   sdo:distribution ?distribution.
 ?distribution conp:formats ?format.
 FILTER (regex(?format, "MINC", "i"))
@@ -41,9 +50,12 @@ example_query_4 = """# The query searches for datasets that have an authorizatio
 
 PREFIX sdo: <https://schema.org/>
 PREFIX conp: <https://reservoir.global/v1/vocabs/Public/CONP/>
+PREFIX nexus: <https://bluebrain.github.io/nexus/vocabulary/>
+
 SELECT DISTINCT ?dataset_name ?value WHERE {
 ?dataset a sdo:Dataset;
   sdo:name ?dataset_name;
+  nexus:deprecated false;
   sdo:distribution ?distribution.
 ?distribution sdo:accessMode ?access_mode.
 ?access_mode conp:authorizations ?authorization.
@@ -55,11 +67,14 @@ FILTER regex(lcase(str(?value)), "public", "i")
 example_query_5 = """# The query returns a list of cited papers (including their doi) with datasets that cited them.
 
 PREFIX sdo: <https://schema.org/>
+PREFIX nexus: <https://bluebrain.github.io/nexus/vocabulary/>
+
 SELECT DISTINCT ?citation_name ?doi
 (GROUP_CONCAT(DISTINCT ?title; separator=" | ") as ?datasets) 
 (COUNT(DISTINCT ?title) as ?citation_count) WHERE {
 ?dataset a sdo:Dataset;
   sdo:name ?title;
+  nexus:deprecated false;
   sdo:citation ?citation.
 ?citation sdo:identifier ?value;
           sdo:name ?citation_name.
