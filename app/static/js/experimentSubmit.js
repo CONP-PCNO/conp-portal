5f1f7737efdc597c728a322214c1e9721ad95edc
joshunrau <contact@joshuaunrau.com>
2022-10-10 15:00:45 -0400

    fix submit ids

--- app/static/js/experimentSubmit.js
+++ app/static/js/experimentSubmit.js
@@ -3,7 +3,10 @@ const addFieldListEntry = (fieldListId) => {
   const listItems = fieldList.getElementsByTagName('li');
   const newListItem = document.createElement('li');
   const newInput = document.createElement('input');
+  const newInputName = `${fieldListId}-${listItems.length}`
   newInput.classList.add('form-control')
+  newInput.name = newInputName
+  newInput.id = newInputName
   fieldList.appendChild(newListItem);
   newListItem.appendChild(newInput);
 }
