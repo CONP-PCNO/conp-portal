a2161b97697e9ba0d9425b59e75b9aced8f76620
joshunrau <contact@joshuaunrau.com>
2022-10-16 14:02:02 -0400

    add css rule

--- app/static/lib/experiments-portal/webpack.config.ts
+++ app/static/lib/experiments-portal/webpack.config.ts
@@ -22,6 +22,10 @@ const config: webpack.Configuration = {
   module: {
     rules: [
       {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
         test: /\.ts?$/,
         use: 'ts-loader',
         exclude: /node_modules/
