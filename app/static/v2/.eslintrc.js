8cacf5be3d699e28edf6bae2d318e68c55a9ca10
joshunrau <contact@joshuaunrau.com>
2023-02-05 13:43:10 -0500

    setup for JS

--- app/static/v2/.eslintrc.js
+++ app/static/v2/.eslintrc.js
@@ -1,27 +1,36 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  ignorePatterns: ['*.js'],
+  ignorePatterns: '*.js',
   env: {
     browser: true,
     es2021: true
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:storybook/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking'
-  ],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:storybook/recommended'],
   plugins: ['@typescript-eslint'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      tsx: true
-    },
-    project: ['./tsconfig.json'],
-    tsconfigRootDir: __dirname
+      jsx: true
+    }
   },
+  overrides: [
+    {
+      files: ['*.tsx'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking'
+      ],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaFeatures: {
+          tsx: true
+        },
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: __dirname
+      }
+    }
+  ],
   settings: {
     react: {
       version: 'detect'
