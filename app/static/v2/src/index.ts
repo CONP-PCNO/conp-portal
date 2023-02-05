aa2c437be929b41725bb2f54f87904340619c2ef
joshunrau <contact@joshuaunrau.com>
2023-02-05 13:15:37 -0500

    refactor home

--- app/static/v2/src/index.ts
+++ app/static/v2/src/index.ts
@@ -1,9 +1,12 @@
 import $ from 'jquery';
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 import './styles/main.scss';
 
 import 'bootstrap';
 import 'popper.js';
 
-export { $, React };
+export { $, React, ReactDOM };
+
+export * from './features';
