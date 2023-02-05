f5e4948b52e78f043490b9688b02ee0819d335be
joshunrau <contact@joshuaunrau.com>
2023-02-05 12:06:13 -0500

    add storybook

--- app/static/v2/.eslintrc.js
+++ app/static/v2/.eslintrc.js
@@ -1,29 +1,30 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  ignorePatterns: ["*.js"],
+  ignorePatterns: ['*.js'],
   env: {
     browser: true,
     es2021: true
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking"
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:storybook/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking'
   ],
-  plugins: ["@typescript-eslint"],
-  parser: "@typescript-eslint/parser",
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       tsx: true
     },
-    project: ["./tsconfig.json"],
+    project: ['./tsconfig.json'],
     tsconfigRootDir: __dirname
   },
   settings: {
     react: {
-      version: "detect"
+      version: 'detect'
     }
   }
 };
