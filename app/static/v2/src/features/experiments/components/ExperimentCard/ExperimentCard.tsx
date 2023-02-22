ce9909d5abf6998ec49d22ca08ef4bf2e861d04e
joshunrau <contact@joshuaunrau.com>
2023-02-22 12:25:54 -0500

    add freezeframe

--- app/static/v2/src/features/experiments/components/ExperimentCard/ExperimentCard.tsx
+++ app/static/v2/src/features/experiments/components/ExperimentCard/ExperimentCard.tsx
@@ -4,6 +4,8 @@ import brain from '@/assets/brain.gif';
 import { type Experiment } from '@/features/experiments/types';
 import { ExperimentCardItem } from './ExperimentCardItem';
 
+import ReactFreezeframe from 'react-freezeframe';
+
 interface ExperimentCardProps {
   titleLink?: boolean;
   experiment: Experiment;
@@ -35,7 +37,7 @@ export const ExperimentCard = ({
       <div className="row">
         <div className="col col-sm-2 d-flex flex-column justify-content-center">
           <div className="container-fluid p-2">
-            <img className="animated-gif img-fluid" alt="experiment image" src={imageFile || brain} />
+            <ReactFreezeframe className="animated-gif img-fluid" alt="experiment image" src={imageFile || brain} />
           </div>
         </div>
         <div className="col col-lg-7 card-body d-flex p-2">
